4feab25d30c2e66a08b3bf162527e74dee8e777c
Vaideha Sathe <sathevaideha@gmail.com>
2025-10-28 20:05:31 +0000

    Removed redundant data and code, modified setup_nltk.py.

--- src/config/setup_nltk.py
+++ src/config/setup_nltk.py
@@ -2,7 +2,7 @@
 
 import nltk
 
-resources = ['punkt', 'averaged_perceptron_tagger', 'wordnet', 'stopwords']
+resources = ['punkt', 'averaged_perceptron_tagger', 'wordnet', 'stopwords', 'punkt_tab', 'averaged_perceptron_tagger_eng']
 
 for resource in resources:
     nltk.download(resource)
