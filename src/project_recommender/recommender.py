a4744724091cc6f7d444fb2a867af6e8d5556a82
Thomas Shaw <thomas.shaw@st-annes.ox.ac.uk>
2025-10-28 16:05:42 +0000

    Adapted recommender so that it returns as many projects as you soon, plus various fixes (see desc)
    - User input must be longer than 15 entries
    - Each recommended project has a number associated to it
    - If the number of suggested projects is less than the requested number, the program flags this, but returns it anyway (to show its not broken)

--- src/project_recommender/recommender.py
+++ src/project_recommender/recommender.py
@@ -1,7 +1,8 @@
 # Date 28/10/2025
 # DESCRIPTION: This function takes a user input as a string,
 # and a .csv file containing tokenized project data,
-# and returns a project that is most similar to the user input
+# and returns a number of projects (decided by the user) which
+# are most similar to the user input string
 
 from preprocessor import query_preprocessor
 import pandas as pd
@@ -12,7 +13,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 tfidfvec = TfidfVectorizer()
 
-def recommend(user_input,tokenized_data_csv):
+def recommend(user_input,tokenized_data_csv,amount_wanted):
     "Takes a user statement and .csv file, and returns the"
     "project title which matches closest to the user statement "
 
@@ -21,26 +22,49 @@ def recommend(user_input,tokenized_data_csv):
 
     "Returns: The project title which is 'closest' to the user statement"
     
-    
-    tokenized_data = pd.read_csv(tokenized_data_csv)
+    #check that the user input is greater than 15 words
+    if len(user_input.split())<=15:
+        return "Your statement is too short!"
+    else:
 
-    #processes the user statement, removes unnecessary words
-    user_tokens = query_preprocessor(user_input)  
+        #read the .csv file
+        tokenized_data = pd.read_csv(tokenized_data_csv)
 
-    #transforms the processed project descriptions into vectorized form    
-    tfidf_data = tfidfvec.fit_transform(tokenized_data['tokenized_description'])
+        #processes the user statement, removes unnecessary words
+        user_tokens = query_preprocessor(user_input)  
 
-    #turn user_tokens into a list which transform will accept
-    user_tokens_list = user_tokens.split(" ")
+        #transforms the processed project descriptions into vectorized form    
+        tfidf_data = tfidfvec.fit_transform(tokenized_data['tokenized_description'])
 
-    #transforms the processed user statement into vectorised form
-    tfidf_user = tfidfvec.transform(user_tokens_list)
+        #turn user_tokens into a list which transform will accept
+        user_tokens_list = user_tokens.split(" ")
 
-    #creates a score between the user statement and project descriptions based on how similar
-    cos_sim = cosine_similarity(tfidf_user, tfidf_data)
+        #transforms the processed user statement into vectorised form
+        tfidf_user = tfidfvec.transform(user_tokens_list)
 
-    #finds the highest score
-    row_best_score, col_best_score = np.unravel_index(cos_sim.argmax(), cos_sim.shape)
+        #creates a score between the user statement and project descriptions based on how similar
+        cos_sim = cosine_similarity(tfidf_user, tfidf_data)
 
-    #returns the title of the project which obtained the highest score
-    return tokenized_data['title'][col_best_score]
+        #adds the scores for each project
+        project_scores = np.sum(cos_sim, axis=0)
+
+        #lists the indices from highest-to-lowest score
+        indices_sorted = np.argsort(project_scores)[::-1]
+        no_zeros = indices_sorted[project_scores[indices_sorted]>0]
+        
+        #keeps the number of amount_wanted
+        no_zeros_correct_amount=no_zeros[:amount_wanted]
+
+        #adds those projects to a list
+        projects=[]
+        for i in no_zeros_correct_amount:
+            projects.append([tokenized_data['title'][i],project_scores[i]])
+        
+        #if the user wanted more than were suggested
+        if len(projects)<amount_wanted:
+            print('There are only',len(projects),'recommendations: ')
+        
+        #change formatting
+        final_projects=np.array(projects)
+
+        return final_projects
