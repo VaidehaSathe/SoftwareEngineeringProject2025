3bd7603e1daa6aa8a343b8d179c6f7990fdd2c85
Thomas Shaw <thomas.shaw@st-annes.ox.ac.uk>
2025-10-28 11:42:42 +0000

    Added first draft of recommender.py
    Currently returns one project - the most similar one to the prompt

--- src/project_recommender/recommender.py
+++ src/project_recommender/recommender.py
@@ -1,9 +1,13 @@
-# Placeholder for recommender script
-# Functionality: (Data.Description) tokenized text + (User Query) tokenized text -> (Recommendations) list of Data.ProjectTitle strings
-
-import pandas as pd
+# Date 28/10/2025
+# DESCRIPTION: This function takes a user input as a string,
+# and a .csv file containing tokenized project data,
+# and returns a project that is most similar to the user input
 
 from preprocessor import query_preprocessor
+import pandas as pd
+import nltk
+import numpy as np
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 tfidfvec = TfidfVectorizer()
@@ -19,21 +23,24 @@ def recommend(user_input,tokenized_data):
 
     #processes the user statement, removes unnecessary words
     user_tokens = query_preprocessor(user_input)  
-    
-    #transforms the processed user statement into vectorised form
-    tfidf_user = tfidfvec.transform(user_tokens)
 
     #transforms the processed project descriptions into vectorized form    
     tfidf_data = tfidfvec.fit_transform(tokenized_data['tokenized_description'])
+
+    #turn user_tokens into a list which transform will accept
+    user_tokens_list = user_tokens.split(" ")
+
+    #transforms the processed user statement into vectorised form
+    tfidf_user = tfidfvec.transform(user_tokens_list)
 
     #creates a score between the user statement and project descriptions based on how similar
     cos_sim = cosine_similarity(tfidf_user, tfidf_data)
 
     #finds the highest score
-    best_score = cos_sim.argmax()
+    row_best_score, col_best_score = np.unravel_index(cos_sim.argmax(), cos_sim.shape)
 
     #returns the title of the project which obtained the highest score
-    return tokenized_data['title'][best_score]
+    return tokenized_data['title'][col_best_score]
 
 
 
