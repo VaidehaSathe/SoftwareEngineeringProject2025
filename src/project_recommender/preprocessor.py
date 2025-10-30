69352908c0e0272b273e2f6469ff013ded2c5faf
Oliver Staples <oliver.staples@hertford.ox.ac.uk>
2025-10-30 16:44:48 +0000

    Made pylint recommended changes

--- src/project_recommender/preprocessor.py
+++ src/project_recommender/preprocessor.py
@@ -1,13 +1,18 @@
-# Date: 27/10/2025 
-#--------------------------------------------------------#
-# Description: This module contains functions to preprocess text data to feed into the recommender module.
-# Functionality: Original Text -> Tokenized Text
-# data_preprocessor: Tokenizes all description elements from Extracted Data CSV
-# query_preprocessor: Tokenizes input text string for recommender
-# NOTE: REQUIRES NLTK DATA TO BE DOWNLOADED. RUN setup_nltk.py IN config FOLDER TO DOWNLOAD.
-#--------------------------------------------------------#
+"""
+preprocessor.py
+Date: 27/10/2025
+--------------------------------------------------------#
+Description: This module contains functions to preprocess text data to feed into
+the recommender module.
+Functionality: Original Text -> Tokenized Text
+data_preprocessor: Tokenizes all description elements from Extracted Data CSV
+query_preprocessor: Tokenizes input text string for recommender
+NOTE: REQUIRES NLTK DATA TO BE DOWNLOADED. RUN setup_nltk.py IN config FOLDER TO DOWNLOAD.
+--------------------------------------------------------#
+"""
+
 import pandas as pd
-import nltk 
+import nltk
 
 nltk_resources=['punkt', 'averaged_perceptron_tagger', 'wordnet', 'stopwords']
 
@@ -29,8 +34,8 @@ def add_nltk_resources():
 add_nltk_resources()
 
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 from nltk.corpus import stopwords
+lemmatizer = WordNetLemmatizer()
 
 stop_words = set(stopwords.words('english'))
 
@@ -113,47 +118,38 @@ def data_preprocessor(filename):
 
     # Save to tokenized_CSVs directory
     dataframe.to_csv(f"data/tokenized_CSVs/{new_filename}", index=False)
-
 # Previous code to delete rows
 # def data_preprocessor(filename):
 #     """
 #     Preprocesses the 'description' column of the input DataFrame.
-    
 #     Args:
 #         CSV File (.csv): The project list CSV name from the data/project_CSVs folder.
 #     Saves:
-#         CSV File (.csv): A new CSV with an additional 'tokenized_description' column in data/tokenized_CSVs folder.
+#         CSV File (.csv): A new CSV with an additional 'tokenized_description'
+#           column in data/tokenized_CSVs folder.
 #     Returns:
 #         None
 #     """
-
 #     # The input (original) file
 #     dataframe = pd.read_csv(f'data/project_CSVs/{filename}')
-
 #     def too_many_rows(row):
 #         text = " ".join(map(str, row.values)).lower()
 #         # Remove row if "empty" appears more than twice
 #         return text.count("empty") > 2
-
 #     before_count = len(dataframe)
 #     # Drop rows where too_many_rows() is True
 #     dataframe = dataframe[~dataframe.apply(too_many_rows, axis=1)]
 #     after_count = len(dataframe)
-
 #     if before_count - after_count != 0:
 #         print(f"🧹 Removed {before_count - after_count} rows containing 'empty' more than twice.")
 #     else:
 #         print("🧹 No need to remove any rows.")
-
 #     # Tokenize the description column
 #     dataframe['tokenized_description'] = dataframe['description'].apply(preprocess_text)
-
 #     # The output (new) file name
 #     new_filename = f"tokenized_{filename}"
-
 #     # Save to tokenized_CSVs directory
 #     dataframe.to_csv(f"data/tokenized_CSVs/{new_filename}", index=False)
-
 def query_preprocessor(query):
     """
     Preprocesses the input query string.
